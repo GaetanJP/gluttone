df557ca582230121e246eb6d970c88d07be59c22
GaetanJP <77470120+GaetanJP@users.noreply.github.com>
2022-08-31 22:12:58 -0400

    ADDED STUFF

---
+++ src/pages/Home.jsx
@@ -0,0 +1,16 @@
+import Veggie from "../components/Veggie";
+import Popular from "../components/Popular";
+
+
+import React from 'react'
+
+function home() {
+  return (
+    <div>
+        <Veggie />
+        <Popular />
+    </div>
+  )
+}
+
+export default home;
